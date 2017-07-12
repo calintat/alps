ce5fd2701eca717cd006dbe505b130954797128d
calintat <calintat@gmail.com>
2017-07-12 19:29:19 +0100

    Added custom DSL for defining preferences

---
+++ library/src/main/java/com/github/calintat/AlpsDsl.kt
@@ -0,0 +1,75 @@
+package com.github.calintat
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.preference.*
+
+@DslMarker
+@Target(AnnotationTarget.TYPE)
+private annotation class AlpsDslMarker
+
+/**
+ * Initializer lambda for a given preference type T.
+ */
+private typealias Init<T> = (@AlpsDslMarker T).() -> Unit
+
+/**
+ * Set an url to be opened when this preference is clicked.
+ */
+fun Preference.setUrl(url: String) {
+
+    intent = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(url) }
+}
+
+/**
+ * Populate the given preference fragment with preferences using the DSL.
+ */
+fun PreferenceFragment.populateWithPreferences(init: Init<PreferenceScreen>) {
+
+    preferenceScreen = preferenceManager.createPreferenceScreen(activity).apply(init)
+}
+
+/**
+ * Populate the given container with a preference fragment generated with the DSL.
+ *
+ * @param containerViewId Identifier of the container where the fragment will be added.
+ */
+fun Activity.populateWithPreferences(containerViewId: Int, init: Init<PreferenceScreen>) {
+
+    class PreferenceFragmentDsl : PreferenceFragment() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+
+            super.onCreate(savedInstanceState); populateWithPreferences(init)
+        }
+    }
+
+    fragmentManager.beginTransaction().replace(containerViewId, PreferenceFragmentDsl()).commit()
+}
+
+@PublishedApi
+internal inline fun <T : Preference> PreferenceGroup.pref(build: (Context) -> T, key: String? = null, init: Init<T>): T {
+
+    return build(context).apply { if (key != null) this.key = key; addPreference(this); init() }
+}
+
+inline fun PreferenceGroup.checkBoxPreference(key: String, init: Init<CheckBoxPreference>) = pref(::CheckBoxPreference, key, init)
+
+inline fun PreferenceGroup.editTextPreference(key: String, init: Init<EditTextPreference>) = pref(::EditTextPreference, key, init)
+
+inline fun PreferenceGroup.ringtonePreference(key: String, init: Init<RingtonePreference>) = pref(::RingtonePreference, key, init)
+
+inline fun PreferenceGroup.switchPreference(key: String, init: Init<SwitchPreference>) = pref(::SwitchPreference, key, init)
+
+inline fun PreferenceGroup.listPreference(key: String, init: Init<ListPreference>) = pref(::ListPreference, key, init)
+
+inline fun PreferenceGroup.multiSelectListPreference(key: String, init: Init<MultiSelectListPreference>) = pref(::MultiSelectListPreference, key, init)
+
+inline fun PreferenceGroup.preference(key: String? = null, init: Init<Preference>) = pref(::Preference, key, init)
+
+inline fun PreferenceGroup.preferenceCategory(key: String? = null, init: Init<PreferenceCategory>) = pref(::PreferenceCategory, key, init)
+
+inline fun PreferenceGroup.preferenceScreen(key: String? = null, init: Init<PreferenceScreen>) = pref(preferenceManager::createPreferenceScreen, key, init)!!
