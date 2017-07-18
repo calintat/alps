142d962a7f0dd7c4915405c4d7bb18fdf48c6d7c
calintat <calintat@gmail.com>
2017-07-18 22:47:12 +0100

    Added custom change listener for each preference type

---
+++ alps-dsl/src/main/java/com/github/calintat/Listeners.kt
@@ -0,0 +1,52 @@
+package com.github.calintat
+
+import android.preference.*
+
+inline fun CheckBoxPreference.onChange(crossinline listener: (Boolean) -> Boolean) {
+
+    setOnPreferenceChangeListener { preference, newValue ->
+
+        preference == this && newValue is Boolean && listener(newValue)
+    }
+}
+
+inline fun EditTextPreference.onChange(crossinline listener: (String) -> Boolean) {
+
+    setOnPreferenceChangeListener { preference, newValue ->
+
+        preference == this && newValue is String && listener(newValue)
+    }
+}
+
+inline fun RingtonePreference.onChange(crossinline listener: (String) -> Boolean) {
+
+    setOnPreferenceChangeListener { preference, newValue ->
+
+        preference == this && newValue is String && listener(newValue)
+    }
+}
+
+inline fun SwitchPreference.onChange(crossinline listener: (Boolean) -> Boolean) {
+
+    setOnPreferenceChangeListener { preference, newValue ->
+
+        preference == this && newValue is Boolean && listener(newValue)
+    }
+}
+
+inline fun ListPreference.onChange(crossinline listener: (String) -> Boolean) {
+
+    setOnPreferenceChangeListener { preference, newValue ->
+
+        preference == this && newValue is String && listener(newValue)
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+inline fun MultiSelectListPreference.onChange(crossinline listener: (Set<String>) -> Boolean) {
+
+    setOnPreferenceChangeListener { preference, newValue ->
+
+        preference == this && newValue is Set<*> && listener(newValue as Set<String>)
+    }
+}
